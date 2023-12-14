20f84c38bcdfe13ae916730553e7c1a5e8292362
LuisSiccardi <84135938+Luis-Siccardi@users.noreply.github.com>
2023-12-14 11:48:21 -0300

    l

--- pruebapoo.py
+++ pruebapoo.py
@@ -1,20 +1,23 @@
 class Rectangulo:
-    def __init__(self,longitud,ancho):
+    def __init__(self,longitud,ancho,):
         self.longitud=longitud
         self.ancho=ancho
 
     def calcular_area(self):
-        self.area=self.longitud*self.ancho
-      return area 
+        self.area=self.longitud *self.ancho
+        return self.area  
     
     def calcular_perimetro(self):
-        self.perimetro=(self.longitud+self.ancho)*2
-       return perimetro
+        self.perimetro=(self.longitud +self.ancho)*2
+        return self.perimetro
     
-<<<<<<< Updated upstream
 obj1= Rectangulo(6,5)
-=======
-obj1= Rectangulo(6,3)
->>>>>>> Stashed changes
 print(obj1.calcular_area())
-print(obj1.calcular_perimetro())        
+print(obj1.calcular_perimetro())      
+
+obj2= Rectangulo(5,6)
+print(obj2.calcular_area())
+print(obj2.calcular_perimetro())
+
+areaTTotal = obj1.calcular_area() + obj2.calcular_area()
+print(areaTTotal)
